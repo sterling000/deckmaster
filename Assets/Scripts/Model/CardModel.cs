a6523615655a8cd4b734c60235e6ef7bae0eb0c6
Brian Sterling <sterling000@gmail.com>
2020-03-31 16:25:54 -0700

    Stubbing out some rough layout and a few model classes

---
+++ Assets/Scripts/Model/CardModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace deckmaster
+{
+    public class CardModel
+    {
+        /// <summary>
+        /// A unique identifier for this card, taken from archidekt.
+        /// </summary>
+        public int id;
+
+        /// <summary>
+        /// Another unique identifier for the card, taken from archidekt.
+        /// </summary>
+        public string uid;
+        
+        /// <summary>
+        /// unknown as of yet, but might have to do with the tags on cards in archidekt.
+        /// </summary>
+        public string options;
+
+        /// <summary>
+        /// todo: Let make this into an enum flag. cause i like readable enum flags, and determine if we require the colorIdentiy property from archidekt as well.
+        /// </summary>
+        public string colors;
+
+        /// <summary>
+        /// The card name as read from oracle.
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// Mana cost in {2}{W} format
+        /// </summary>
+        public string cost;
+
+        /// <summary>
+        /// Converted mana cost
+        /// </summary>
+        public int cmc;
+
+        /// <summary>
+        /// Do i own the card or not.
+        /// </summary>
+        public bool owned;
+
+        /// <summary>
+        /// Which category it belongs to in the deck i.e. draw, interaction, ramp, land
+        /// </summary>
+        public string category;
+    }
+}
+
---
+++ Assets/Scripts/Model/DeckModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace deckmaster
+{
+    public class DeckModel
+    {
+        /// <summary>
+        /// The unique deck id from archidekt.
+        /// </summary>
+        public int id;
+
+        /// <summary>
+        /// the unique account id of the owner of the deck.
+        /// </summary>
+        public int ownerId;
+
+        /// <summary>
+        /// A list of cards for the deck.
+        /// </summary>
+        public List<CardModel> cards;
+
+        /// <summary>
+        /// The name of the deck
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// A description of the deck.
+        /// </summary>
+        public string description;
+
+        /// <summary>
+        /// The last time the deck was updated on the server
+        /// </summary>
+        public DateTime updatedAt;
+
+        // todo add the colors of the deck here.
+    }
+}
